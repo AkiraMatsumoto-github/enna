8bda147aad5f03c9e9e8c6c5237c31b0e1a2d31b
AUDmatsumoto <AkiraMatsumoto@users.noreply.github.com>
2026-01-08 21:03:04 +0900

    floating

---
+++ remove_bg.py
@@ -0,0 +1,25 @@
+from PIL import Image
+
+def remove_white_background(input_path, output_path):
+    img = Image.open(input_path)
+    img = img.convert("RGBA")
+    datas = img.getdata()
+
+    new_data = []
+    for item in datas:
+        # Change all white (also shades of whites)
+        # to transparent
+        if item[0] > 240 and item[1] > 240 and item[2] > 240:
+            new_data.append((255, 255, 255, 0))
+        else:
+            new_data.append(item)
+
+    img.putdata(new_data)
+    img.save(output_path, "PNG")
+    print("Background removed successfully.")
+
+if __name__ == "__main__":
+    remove_white_background(
+        "/Users/matsumotoakira/Documents/Private_development/enna/public/images/floating_baby.png",
+        "/Users/matsumotoakira/Documents/Private_development/enna/public/images/floating_baby_transparent.png"
+    )
